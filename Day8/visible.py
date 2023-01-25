d347579f4fd55086d02c52f39105196a8193778b
Bright <76822059+minecrafting-123@users.noreply.github.com>
2023-01-25 18:02:27 +0000

    i think this works?

--- Day8/visible.py
+++ Day8/visible.py
@@ -11,10 +11,29 @@ open ("input.txt") as reader:
 
 def beegCheck(x, y):
     height = coords[y][x]
+    checksFailed = 0
     if (x == 0 or x == coords[0].len() or y == 0 or y == coords.len()):
         return true
-    for i in range(x, 0, -1):
+    for i in range(0, x, 1):
         if (coords[y][i] >= height):
+            checksFailed += 1
+            break
+    for j in range(x+1, coords.len(), 1):
+        if (coords[y][j] >= height):
+            checksFailed += 1
+            break
+    for k in range(0, y, 1):
+        if (coords[k][x] >= height):
+            checksFailed += 1
+            break
+    for l in range(y+1, coords.len(), 1):
+        if (coords[l][x] >= height):
+            checksFailed += 1
+            break
+    if (checksFailed == 4):
+        return false
+    return true
+
 
 #goes through all values and checks for visibility
 for i in range(coords.len()):
