43f0da90248771e801bacf067c43e131c7910309
Bright <76822059+minecrafting-123@users.noreply.github.com>
2023-02-24 00:59:44 +0000

    sets and tuples

---
+++ Day9/long.py
@@ -0,0 +1,4 @@
+#copy follow code from rope.py
+#assign each rope a "lead" in class
+#head follows commands
+#have a "visited" for only the last knot
--- Day9/rope.py
+++ Day9/rope.py
@@ -1,7 +1,7 @@
-import re
+import re, math
 order = re.compile("[A-Z] [0-9]+")
 
-visited = {}
+visited = set()
 
 class Rope:
     def __init__(self, x, y):
@@ -11,17 +11,47 @@ class Rope:
         return self.x
     def getY(self):
         return self.y
-    def move(self, x, y):
-        self.x = x
-        self.y = y
+    def move(self, xChange, yChange):
+        self.x = self.x + xChange
+        self.y = self.y + yChange
 
 head = Rope(0, 0)
 tail = Rope(0, 0)
 
-with open("test.txt") as reader:
+with open("input.txt") as reader:
     for line in reader:
         inp = order.findall(line)[0].split(" ")
-        direction, distance = inp[0], inp[1]
+        direction, distance = inp[0], int(inp[1])
         for i in range(distance):
             if (direction == "L"):
-                
+                head.move(-1, 0)
+            elif (direction == "R"):
+                head.move(1, 0)
+            elif (direction == "U"):
+                head.move(0, 1)
+            elif (direction == "D"):
+                head.move(0, -1)
+            xMove = head.getX() - tail.getX()
+            yMove = head.getY() - tail.getY()
+
+            if (xMove ** 2 + yMove ** 2 > 4):
+                if (xMove > 0 and yMove > 0):
+                    tail.move(1, 1)
+                elif (xMove > 0 and yMove < 0):
+                    tail.move(1, -1)
+                elif (xMove < 0 and yMove > 0):
+                    tail.move(-1, 1)
+                elif (xMove < 0 and yMove < 0):
+                    tail.move(-1,-1)
+            elif(xMove ** 2 + yMove ** 2 > 2):
+                if (xMove > 0):
+                    tail.move(1, 0)
+                elif (xMove < 0):
+                    tail.move(-1, 0)
+                elif (yMove > 0):
+                    tail.move(0, 1)
+                elif (yMove < 0):
+                    tail.move(0, -1)
+            visited.add((tail.getX(), tail.getY()))
+
+print(len(visited))
