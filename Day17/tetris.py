f97ee555f2b80f584a13a214a4385905c0995ba1
minecrafting-123 <bright.zheng@thinksign.com>
2023-06-04 15:04:23 +0800

    psudocode helps me

--- Day17/tetris.py
+++ Day17/tetris.py
@@ -35,31 +35,12 @@ def renderRock(coords, rock):
     for i, row in enumerate(rock):
         for j, val in enumerate(row):
             field[coords[0] + i, coords[1] + j] = 1
-def fall(coords, rock, direction):
+def check(coords, rock):
     blocked = False
-    movedCoords = coords
     for i, row in enumerate(rock):
         for j, val in enumerate(row):
-            #If it goes out of bounds on Y axis
-            if (coords[0] + len(rock) - i > len(field) - 1):
+            if not all(-1 < x < y for x, y, in zip(coords, (10000, 7))) or field[coords[0] + i, coords[1] + j] == 1:
                 blocked = True
                 break
-            #If it goes out of bounds on X axis
-            if (coords[1] + len(row) - j < 0 or coords[1] + len(row) - j >= 7):
-                blocked = True
-                break
-            #If there is another rock in the way
-            if (field[coords[0] + len(rock) - i, coords[1] + len(row) - j] == 1):
-                blocked = True
-                break
-    #Gust blows
-    if (coords[1] + direction >= 0 and coords[1] + direction < 7):
-        gusted = True
-        for i, row in enumerate(rock):
-            if (row.find(1) != -1):
-                if()
-        movedCoords = tuple(coords[0], coords[1] - 1)
-    if (blocked):
-        renderRock(coords, rock)
-    else:
-        fall((coords[0] + 1, coords[1] + 1), rock)
+    return blocked
+        
