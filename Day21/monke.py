8888d96f7dbe1620ff328c16c2ce46a600551c10
minecrafting-123 <bright.zheng@thinksign.com>
2023-07-01 21:28:28 +0800

    china updates

--- Day21/monke.py
+++ Day21/monke.py
@@ -10,15 +10,42 @@ with open("input.txt", "r") as reader:
 
 def get(monkey):
     #if it's a number monkey
-    print(monkey)
+    #print(monkey)
     if re.findall("^(\d+)", monkeybook[monkey]):
         return int(monkeybook[monkey])
-    monkeyParts = re.findall("^([a-z]+) (.) ([a-z]+)", monkeybook[monkey])[0]
-    print(monkeyParts)
-    
-    print(monkeybook[monkey])
-    return eval()
+    #if it's an operation monkey...
+    #find the component monkeys
+    monkeyParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook[monkey])[0]
+    #print(monkeyParts)
+    #calculate the values of the component monkeys
+    locals()[monkeyParts[0]] = get(monkeyParts[0])
+    locals()[monkeyParts[1]] = get(monkeyParts[1])
+    #print(monkeybook[monkey])
+    return eval(monkeybook[monkey])
 
-print(get("fchg"), get("hjpd"))
+print(get("root"))
 
 #eval() is the literal execution
+#try1: 70674280581468 FIRST TRY BABYYYYYYYY
+# RECURSION FINALLY WORKED INSTEAD OF OVERLOADING WE WIN THESEEEEEEE
+def get2(monkey):
+    #if it's a number monkey
+    #print(monkey)
+    if re.findall("^(\d+)", monkeybook[monkey]):
+        return int(monkeybook[monkey])
+    #if it's an operation monkey...
+    #find the component monkeys
+    monkeyParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook[monkey])[0]
+    #print(monkeyParts)
+
+    #calculate the values of the component monkeys
+    locals()[monkeyParts[0]] = get(monkeyParts[0])
+    locals()[monkeyParts[1]] = get(monkeyParts[1])
+    #print(monkeybook[monkey])
+    return eval(monkeybook[monkey])
+#part 2
+#need to find the root half that doesn't include "humn" so i can try to set equal
+rootParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook["root"])[0]
+
+#how to do reverse calculations? some kind of indicator if a "route" includes "humn" maybe?
+#man, if i could set a mathematical variable wouldn't that be nice... maybe i can somehow
