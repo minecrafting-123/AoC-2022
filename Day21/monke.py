718a3c5fa55f3b823bf5c33e9f89b888afd1c3bc
minecrafting-123 <bright.zheng@thinksign.com>
2023-07-03 18:35:10 +0800

    forgot to change an index :pensive_motion:

--- Day21/monke.py
+++ Day21/monke.py
@@ -12,7 +12,7 @@ def get(monkey):
     #if it's a number monkey
     #print(monkey)
     if re.findall("^(\d+)", monkeybook[monkey]):
-        return int(monkeybook[monkey])
+        return float(monkeybook[monkey])
     #if it's an operation monkey...
     #find the component monkeys
     monkeyParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook[monkey])[0]
@@ -28,24 +28,73 @@ print(get("root"))
 #eval() is the literal execution
 #try1: 70674280581468 FIRST TRY BABYYYYYYYY
 # RECURSION FINALLY WORKED INSTEAD OF OVERLOADING WE WIN THESEEEEEEE
+
+#part 2
+operationList = []
+rootnum = 0
 def get2(monkey):
     #if it's a number monkey
     #print(monkey)
+    if monkey == "humn":
+        return "humn"
     if re.findall("^(\d+)", monkeybook[monkey]):
-        return int(monkeybook[monkey])
+        return float(monkeybook[monkey])
     #if it's an operation monkey...
-    #find the component monkeys
-    monkeyParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook[monkey])[0]
+    #find the component monkeys and operation
+    monkeyParts = re.findall("^([a-z]+) (.) ([a-z]+)", monkeybook[monkey])[0]
+    #first monkey, operation, second monkey
     #print(monkeyParts)
-
     #calculate the values of the component monkeys
-    locals()[monkeyParts[0]] = get(monkeyParts[0])
-    locals()[monkeyParts[1]] = get(monkeyParts[1])
-    #print(monkeybook[monkey])
+    locals()[monkeyParts[0]] = get2(monkeyParts[0])
+    locals()[monkeyParts[2]] = get2(monkeyParts[2])
+    #print(locals()[monkeyParts[0]], locals()[monkeyParts[2]])
+    #if "humn" then record operation being applied
+    if locals()[monkeyParts[0]] == "humn":
+        operationList.append((get(monkeyParts[2]), monkeyParts[1]))
+        return "humn"
+    if locals()[monkeyParts[2]] == "humn":
+        if (monkeyParts[1] == "-"):
+            operationList.append((get(monkeyParts[0]), "-."))
+            return "humn"
+        if (monkeyParts[1] == "/"):
+            operationList.append((get(monkeyParts[0]), "/."))
+            return "humn"
+        operationList.append((get(monkeyParts[0]), monkeyParts[1]))
+        return "humn"
+    #print(monkeybook[monkey]
     return eval(monkeybook[monkey])
-#part 2
-#need to find the root half that doesn't include "humn" so i can try to set equal
-rootParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook["root"])[0]
 
-#how to do reverse calculations? some kind of indicator if a "route" includes "humn" maybe?
-#man, if i could set a mathematical variable wouldn't that be nice... maybe i can somehow
+def reverseOperation(partnum, operation):
+    global rootnum
+    #x + p = r
+    if operation == "+":
+        rootnum -= partnum
+    if operation == "-":
+        rootnum += partnum
+    if operation == "-.":
+        rootnum = partnum - rootnum
+    if operation == "*":
+        rootnum /= partnum
+    if operation == "/":
+        rootnum *= partnum
+    if operation == "/.":
+        rootnum = partnum / rootnum
+
+rootParts = re.findall("^([a-z]+) . ([a-z]+)", monkeybook["root"])[0]
+root1, root2 = get2(rootParts[0]), get2(rootParts[1])
+print(root1, root2)
+if (root1 != "humn"):
+    rootnum = root1
+else:
+    rootnum = root2
+print(rootnum)
+
+while operationList:
+    partnum, operation = operationList.pop()
+    print(rootnum, operation, partnum)
+    reverseOperation(partnum, operation)
+print(rootnum)
+
+#try 1: 51164504202966 (too high)
+#try 2:-1089.1550203110228 (i mean this is a decimal so no)
+#try 3: 3243420789721 (I FORGOT TO CHANGE A NUMBER WHEN COPY AND PASTING)
